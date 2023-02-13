7cc882f6bbec68e6749bea69f5903a45b9d3a0e7
Xuelei Li <lix@mediamarktsaturn.com>
2023-02-13 13:38:41 +0100

    Fix comment

--- datahub-web-react/src/conf/Integration.ts
+++ datahub-web-react/src/conf/Integration.ts
@@ -1,5 +1,5 @@
 export enum IntegrationRoutes {
-    // TODO How to configure EAM URL can be configured via Helm values
+    // TODO How to configure EAM URL? Possible via Helm values? Via custom build process?
     // NOTE Configure your reverse proxy or load balancer to forward requests to proper upstream
     ENTITY_ACCESS_MANAGEMENT = '/eam',
 }
